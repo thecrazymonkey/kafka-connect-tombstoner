f53b0ce813af7b9dd0dd28e8f1f5a4310a689aed
Ivan Kunz <thecrazymonkey@users.noreply.github.com>
2022-12-07 09:11:44 -0500

    fix header

--- src/test/java/com/github/thecrazymonkey/kafka/connect/smt/tombstoner/TombstonerTest.java
+++ src/test/java/com/github/thecrazymonkey/kafka/connect/smt/tombstoner/TombstonerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019 Christopher Matta (chris.matta@gmail.com)
+ * Copyright © 2022 Ivan Kunz
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
